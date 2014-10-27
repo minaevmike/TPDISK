8a69ca5b4b10d946c32ceecefd2a49942e289490
alexvasiliev <alexvasiliev92@gmail.com>
2014-10-27 16:39:43 +0300

    added json pars 4 get resourses

--- app/src/main/java/com/example/mike/tpdisk/FileInstanse.java
+++ app/src/main/java/com/example/mike/tpdisk/FileInstanse.java
@@ -4,13 +4,130 @@ package com.example.mike.tpdisk;
  * Created by Mike on 26.10.2014.
  */
 public class FileInstanse {
-    public String size, public_key, origin_size, name, created, public_url,
-    modified, path, media_type, preview, type, mime_type, md5;
 
-    public FileInstanse(String size, String public_key, String origin_size, String name, String created, String public_url, String modified, String path, String media_type, String preview, String type, String mime_type, String md5) {
+    private int size;
+    private String public_key;
+    private String origin_path;
+    private String name;
+    private String created;
+    private String public_url;
+    private String modified;
+    private String path;
+    private String media_type;
+    private String preview;
+    private String type;
+    private String mime_type;
+    private String md5;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getPublic_key() {
+        return public_key;
+    }
+
+    public String getOrigin_path() {
+        return origin_path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public String getPublic_url() {
+        return public_url;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public String getPreview() {
+        return preview;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMime_type() {
+        return mime_type;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setPublic_key(String public_key) {
+        this.public_key = public_key;
+    }
+
+    public void setOrigin_path(String origin_path) {
+        this.origin_path = origin_path;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public void setPublic_url(String public_url) {
+        this.public_url = public_url;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setMedia_type(String media_type) {
+        this.media_type = media_type;
+    }
+
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMime_type(String mime_type) {
+        this.mime_type = mime_type;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public FileInstanse(int size, String public_key, String origin_path, String name, String created, String public_url,
+                        String modified, String path, String media_type, String preview, String type, String mime_type, String md5) {
         this.size = size;
         this.public_key = public_key;
-        this.origin_size = origin_size;
+        this.origin_path = origin_path;
         this.name = name;
         this.created = created;
         this.public_url = public_url;
@@ -21,5 +138,11 @@ public class FileInstanse {
         this.type = type;
         this.mime_type = mime_type;
         this.md5 = md5;
+    }
+
+    public FileInstanse(){
+        this.size = -1;
+        this.public_key = this.origin_path = this.name = this.created = this.public_url = this.modified = this.path =
+        this.media_type = this.preview = this.type = this.mime_type = this.md5 = "";
     }
 }
--- app/src/main/java/com/example/mike/tpdisk/JsonFileListParser.java
+++ app/src/main/java/com/example/mike/tpdisk/JsonFileListParser.java
@@ -1,15 +1,20 @@
 package com.example.mike.tpdisk;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
  * Created by Mike on 26.10.2014.
  */
 public class JsonFileListParser {
+    public static final String ADDRESS = "https://cloud-api.yandex.net/v1/";
     private String getOrNull(JSONObject object, String key){
         try {
             return object.getString(key);
@@ -18,13 +23,29 @@ public class JsonFileListParser {
         }
     }
     public List<FileInstanse> parse(String s) {
+
         List<FileInstanse> list = null;
         try{
             JSONObject reader = new JSONObject(s);
             JSONObject embeded = reader.getJSONObject("_embedded");
             JSONArray files = embeded.getJSONArray("items");
             for(int i = 0;i < files.length();i++){
-                list.add(i,new FileInstanse());
+                //FileInstanse temp = new FileInstanse();
+                int size = files.getJSONObject(i).getInt("size");
+                String public_key = files.getJSONObject(i).getString("public_key");
+                String origin_path  = files.getJSONObject(i).getString("origin_path");
+                String name   = files.getJSONObject(i).getString("name");
+                String created  = files.getJSONObject(i).getString("created");
+                String public_url  = files.getJSONObject(i).getString("public_url");
+                String modified  = files.getJSONObject(i).getString("modified");
+                String path  = files.getJSONObject(i).getString("path");
+                String media_type = files.getJSONObject(i).getString("media_type");
+                String preview  = files.getJSONObject(i).getString("preview");
+                String type  = files.getJSONObject(i).getString("type");
+                String mime_type  = files.getJSONObject(i).getString("mime_type");
+                String md5  = files.getJSONObject(i).getString("md5");
+                list.add(i,new FileInstanse(size, public_key, origin_path, name, created, public_url,
+                        modified, path, media_type, preview, type, mime_type, md5 ));
             }
         }
         catch (JSONException e){
